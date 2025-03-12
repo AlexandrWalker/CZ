c80edd6257393e0c7d35287b960489e11f8ec0d6
AlexandrWalker <alexandr.d.walker@gmail.com>
2025-03-12 21:12:57 +0700

    add all blocks & sliders

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,74 @@
 (() => {
   document.addEventListener('DOMContentLoaded', () => {
 
+    var product__slider = new Swiper(".product__slider-init", {
+      spaceBetween: 30,
+      slidesPerView: 3,
+      // slidesPerGroup: 1,
+      speed: 600,
+      mousewheel: {
+        forceToAxis: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      // breakpoints: {
+      //   601: {
+      //     slidesPerView: 2,
+      //     spaceBetween: 10,
+      //   },
+      //   769: {
+      //     slidesPerView: 3,
+      //     spaceBetween: 20,
+      //   },
+      //   992: {
+      //     slidesPerView: 4,
+      //     spaceBetween: 20,
+      //   },
+      // },
+    });
 
+    function accordionFunc() {
+      var worthHead = document.querySelectorAll('.worth__item'),
+        worthActive = document.getElementsByClassName('active'),
+
+        worthRePrev = document.getElementsByClassName('re-prev'),
+        worthPrev = document.getElementsByClassName('prev'),
+        worthNext = document.getElementsByClassName('next');
+
+      Array.from(worthHead).forEach(function (worthItem, i, worthHead) {
+        worthItem.addEventListener('click', function (e) {
+          if (worthActive.length > 0 && worthActive[0] !== this) {
+            worthActive[0].classList.remove('active');
+          }
+          if (worthRePrev.length > 0) {
+            worthRePrev[0].classList.remove('re-prev');
+          }
+          if (worthPrev.length > 0) {
+            worthPrev[0].classList.remove('prev');
+          }
+          if (worthNext.length > 0) {
+            worthNext[0].classList.remove('next');
+          }
+          this.classList.add('active');
+
+          var prePrev = this.previousElementSibling;
+
+          if (this.previousElementSibling) {
+            this.previousElementSibling.classList.add('prev');
+
+            if (prePrev.previousElementSibling) {
+              prePrev.previousElementSibling.classList.add('re-prev');
+            }
+          }
+          if (this.nextElementSibling) {
+            this.nextElementSibling.classList.add('next');
+          }
+        });
+      });
+    }
+
+    accordionFunc();
   });
 })();
